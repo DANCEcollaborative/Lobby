d458def8cb97d9263986fb45aa464284dd1b1815
rcmurray <>
2023-09-22 15:10:02 -0400

    Bump next room number

--- lobby-direct.py
+++ lobby-direct.py
@@ -48,7 +48,7 @@ TIMEOUT_RESPONSE_CODE = 503
 
 # GLOBAL VARIABLES
 assigner_initialized = False
-nextRoomNum = 7762
+nextRoomNum = 7801
 nextThreadNum = 0
 nextCheckForOldUsers = time.time() + CHECK_FOR_USER_DELETION_WAIT_TIME
 nextCheckForOldRooms = time.time() + CHECK_FOR_ROOM_DELETION_WAIT_TIME
